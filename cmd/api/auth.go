c1129d3228418f692534bad4090717a62455a21d
kumarpramodlohar <kumarpramod.asn@gmail.com>
2025-01-12 20:00:25 +0530

    initial commit

---
+++ cmd/api/auth.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+type Auth struct {
+	Issuer string
+	Audience string
+	Secret string
+	TokenExpiry time.Duration
+	RefereshExpiry time.Duration
+	CookieDomain string
+	CookiePath string
+	CookieName string
+}
+
+type jwtUser struct {
+	ID int `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName string `json:"last_name"`
+}
+
+type TokenPairs struct {
+	Token string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}
+
+type Claims struct {
+	jwt.RegisteredClaims
+}
+
+func (j *Auth) GenerateTokenPair(user *jwtUser) (TokenPairs, error) {
+	// Create a token
+	token := jwt.New(jwt.SigningMethodHS256)
+	// Set the claims
+	claims := token.Claims.(jwt.MapClaims)
+	claims["name"]= fmt.Sprintf("%s %s", user.FirstName, user.LastName)
+	claims["sub"]= fmt.Sprint(user.ID)
+	claims["aud"] = j.Audience
+	claims["iss"] = j.Issuer
+	claims["iat"] = time.Now().UTC().Unix()
+	claims["typ"] = "JWT"
+	// Set the expiry for JWT
+	claims["exp"]= time.Now().UTC().Add(j.TokenExpiry).Unix()
+	// Create a signed token
+	signedAccessToken, err := token.SignedString([]byte(j.Secret))
+	if err != nil {
+		return TokenPairs{}, err
+	}
+	// Create a refresh token and set claims
+	refereshToken := jwt.New(jwt.SigningMethodHS256)
+	refereshTokenClaims := refereshToken.Claims.(jwt.MapClaims)
+	refereshTokenClaims["sub"] = fmt.Sprint(user.ID)
+	refereshTokenClaims["iat"] = time.Now().UTC().Unix()
+
+	// Set the expiry for the refresh token
+	refereshTokenClaims["exp"] = time.Now().UTC().Add(j.RefereshExpiry).Unix()
+	// Create signed refresh token
+	signedRefreshToken, err := refereshToken.SignedString([]byte(j.Secret))
+	if err != nil {
+		return TokenPairs{},err
+	}
+
+	// Create TokenPairs and populate with sign tokens
+	var tokenPairs = TokenPairs {
+		Token: signedAccessToken,
+		RefreshToken: signedRefreshToken,
+	}
+	// Return TokenPairs
+	return tokenPairs, nil
+}
+
+func (j *Auth) GetRefereshCookie(refreshToken string) *http.Cookie {
+	return &http.Cookie{
+		Name: j.CookieName,
+		Path: j.CookiePath,
+		Value: refreshToken,
+		Expires: time.Now().Add(j.RefereshExpiry),
+		MaxAge: int(j.RefereshExpiry.Seconds()),
+		SameSite: http.SameSiteStrictMode,
+		Domain: j.CookieDomain,
+		HttpOnly: true,
+		Secure: true,
+	}
+}
+
+func (j *Auth) GetExpiredRefereshCookie() *http.Cookie {
+	return &http.Cookie{
+		Name: j.CookieName,
+		Path: j.CookiePath,
+		Value: "",
+		Expires: time.Unix(0,0),
+		MaxAge: -1,
+		SameSite: http.SameSiteStrictMode,
+		Domain: j.CookieDomain,
+		HttpOnly: true,
+		Secure: true,
+	}
+}
+
+func (j *Auth) GetTokenFromHeaderAndVerify(w http.ResponseWriter, r *http.Request) (string, *Claims, error) {
+	w.Header().Add("Vary", "Authorization")
+	// get auth header
+
+	authHeader := r.Header.Get("Authorization")
+
+	// sanity check
+
+	if authHeader == "" {
+		return "", nil, errors.New("no auth header")
+	}
+
+	// split the header on spaces
+
+	headerParts := strings.Split(authHeader, " ")
+
+	if (len(headerParts) !=2) {
+		return "", nil, errors.New("invalid auth header")
+
+	}
+
+	// check to see if we have the word Bearer
+
+	if headerParts[0] != "Bearer" {
+		return "", nil, errors.New("invalid auth header")
+	}
+
+	token := headerParts[1]
+
+	// declare an empth claims
+
+	claims := &Claims{}
+
+	// parse the token
+
+	_, err := jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(j.Secret), nil
+	})
+
+	if err != nil {
+		if strings.HasPrefix(err.Error(), "token is expired by") {
+			return "", nil, errors.New("expired token")
+		}
+
+		return "", nil, err
+	}
+
+	if claims.Issuer != j.Issuer {
+		return "", nil, errors.New("invalid issuer")
+	}
+
+	return token, claims, nil
+} 
